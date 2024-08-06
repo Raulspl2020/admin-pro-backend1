866d87f818d2daac76ba4e2d27cf44debef114fc
Raulspl2020 <152801486+Raulspl2020@users.noreply.github.com>
2024-08-06 16:18:47 -0500

    CORS Y EXPRES -Primer commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();//Mantiene información sensible fuera del código fuente .env
+const express=require('express');
+const cors = require('cors')// Acepte solicitudes desde diferentes dominios
+
+const {dbconexion}=require('./database/config')
+
+const app=express();
+
+// configurar CORS
+app.use(cors())
+
+//BASE DE DATOS
+dbconexion();
+
+//console.log(process.env);
+//QAZRFV1209
+//hospital_user
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo',
+    })
+})
+
+
+
+app.listen( process.env.DB_PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.DB_PORT );
+});
